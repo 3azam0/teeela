90e9828a120dc3bca68386f2af061fe1e89548bc
3azam0 <3azam.rnd@gmail.com>
2022-08-30 22:27:53 +0200

    removing warnning

--- src/Services/api.js
+++ src/Services/api.js
@@ -25,8 +25,7 @@ const baseQueryWithInterceptor = async (args, api, extraOptions) => {
   if (result.error && result.error.status === 401) {
     // here you can deal with 401 error
   }
-  console.warn('result', result);
-  const newRes = result.data.map(itm => itm.id);
+ 
   return result;
 };
 
